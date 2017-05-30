2622ca62d74817ef6f774eda1e0658dc76fa5b5c
Rafael da S.P. de Azevedo <rafaelsilva.fatec@gmail.com>
2017-05-30 18:36:29 -0300

    Player and Enemy basic movies working

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ var Enemy = function(x,y,speed) {
     // Setting the Enemy initial location and speed
     this.x = x;
     this.y = y;
-    this.speed = spedd;
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -29,7 +29,7 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
+// Now write your own Player class
 // This class requires an update(), render() and
 // a handleInput() method.
 // The Player class
@@ -41,16 +41,16 @@ var Player = function(x,y) {
     this.y = y;
     // The image/sprite for our players, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/char-boy.pgn';
+    this.sprite = 'images/char-boy.png';
 };
 
 // Update the players's position, required method for game
 // Parameter: dt, a time delta between ticks
 Player.prototype.update = function() {
     // Updates the Player location
-    this.x = x;
-    this.y = y;
-    // Handles collision with the Player
+    this.x;
+    this.y;
+    // Handles collision with the Enemy
 
 };
 
@@ -66,24 +66,25 @@ Player.prototype.handleInput = function(input) {
     //Recall that the player cannot move off screen (so you will need to check for that and handle appropriately).
     //If the player reaches the water the game should be reset by moving the player back to the initial location (you can write a separate reset Player method to handle that).
     if (input === 'left') {
-        this.x -= 1;
+        this.x -= 100;
     }
     if (input === 'up') {
-        this.y += 1;
+        this.y -= 80;
     }
     if (input === 'right') {
-        this.x += 1;
+        this.x += 100;
     }
     if (input === 'down') {
-        this.y -= 1;
+        this.y += 80;
     }
 };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [new Enemy(2,2,2),new Enemy(5,4,2)];
-var player = new Player(1,1);
+var allEnemies = [new Enemy(5,2,10),new Enemy(6,100,20),new Enemy(6,200,20),new Enemy(-6,200,20,new Enemy(-10,200,20))];
+var player = new Player(100,300);
+console.log(player.x+" "+player.y);
 
 
 // This listens for key presses and sends the keys to your
